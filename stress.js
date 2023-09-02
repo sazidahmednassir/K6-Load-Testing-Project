35fd5bdfe7e5aafdbfb711f15f3b364a95a57c83
sazidahmednassir <sazidahmednassir@gmail.com>
2023-09-02 14:06:16 +0600

    first commit

---
+++ stress.js
@@ -0,0 +1,23 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+
+export let options = {
+    stages: [
+      { duration: '10m', target: 200 }, 
+    ],
+    thresholds: {
+      'http_req_duration': ['p(95)<3000'], 
+      'http_req_failed': ['rate<0.01'],    
+    },
+  };
+
+export default function () {
+  const baseUrl="https://simple-books-api.glitch.me/";
+  const endPoint="books";  
+  const res=http.get(`${baseUrl}${endPoint}`);
+  //console.log(res);
+  check(res, { 'status was 200': (r) => r.status === 200})
+    
+  sleep(1);
+}
